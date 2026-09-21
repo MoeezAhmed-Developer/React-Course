0742fc6623c967fe60333f2edd8038ffb25e38e1
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-21 16:33:33 +0500

    add header for gender selection in RadioDropdown component

--- src/components/radio.jsx
+++ src/components/radio.jsx
@@ -2,6 +2,7 @@ function RadioDropdown() {
   return (
     <div>
       <h1>Handle Radio and Dropdown</h1>
+      <h4>Select Gender:</h4>
 
       <input type="radio" name="gender" id="male" />
       <label htmlFor="male">Male</label>
