0bb0fb24bf88e9ecd6e0ead3b3f88c0070be090c
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-21 13:45:49 +0500

    add test component

---
+++ src/components/Test.jsx
@@ -0,0 +1,14 @@
+function TestComponent() {
+  return (
+    <div>
+      <h1>Test Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, aperiam
+        repellat. Optio ut minus mollitia vero quos praesentium quas assumenda!
+      </p>
+      <a href="#">Learn More</a>
+    </div>
+  );
+}
+
+export default TestComponent;
