0fbbfb5db3d69954e1b680818d3f69010ef09477
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-21 16:37:02 +0500

    implement radio selection functionality in RadioDropdown component

--- src/components/radio.jsx
+++ src/components/radio.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 function RadioDropdown() {
+  const [radio, setRadio] = useState("");
   return (
     <div>
       <h1>Handle Radio and Dropdown</h1>
       <h4>Select Gender:</h4>
 
-      <input type="radio" name="gender" id="male" />
+      <input
+        type="radio"
+        name="gender"
+        id="male"
+        onChange={(evt) => setRadio(evt.target.value)}
+      />
       <label htmlFor="male">Male</label>
 
       <input type="radio" id="female" name="gender" />
       <label htmlFor="female">Female</label>
+
+      <h4>Radio Value: {radio}</h4>
     </div>
   );
 }
