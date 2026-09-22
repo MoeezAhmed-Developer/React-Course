b1cb699978aa0d001cc8ba4ffd08b5e5cd00b3bd
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-22 20:33:18 +0500

    feat: add multiple StyledBtn components to StyledComponent for enhanced interactivity

--- src/components/StyledComponent.jsx
+++ src/components/StyledComponent.jsx
@@ -7,10 +7,22 @@ function StyledComponent() {
     text-decoration: underline wavy green;
   `;
 
+  const StyledBtn = styled.button({
+    backgroundColor: "green",
+    color: "white",
+    border: "none",
+    outline: "none",
+    padding: "10px",
+    margin: "10px",
+  });
+
   return (
     <div>
       <h1>Style with Styled Component React</h1>
       <Heading>Styled Heading</Heading>
+      <StyledBtn>Click here</StyledBtn>
+      <StyledBtn>Tap here</StyledBtn>
+      <StyledBtn>Click me</StyledBtn>
     </div>
   );
 }
