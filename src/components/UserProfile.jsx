902329c77893dea03d0a40e2231d8580b8682f6e
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-22 19:55:50 +0500

    feat: restructure UserProfile component layout for improved readability

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -2,12 +2,15 @@ import styles from "../css/user-profile.module.css";
 
 function UserProfile() {
   return (
-    <div className={styles.profile}>
-      <h2>Muhammad Moeez</h2>
-      <p>Frontend Developer</p>
-      <p>Email: moeez@example.com</p>
-      <p>Location: Karachi, Pakistan</p>
-      <button>View Profile</button>
+    <div>
+      <h1>Style with css module</h1>
+      <div className={styles.profile}>
+        <h2>Muhammad Moeez</h2>
+        <p>Frontend Developer</p>
+        <p>Email: moeez@example.com</p>
+        <p>Location: Karachi, Pakistan</p>
+        <button>View Profile</button>
+      </div>
     </div>
   );
 }
