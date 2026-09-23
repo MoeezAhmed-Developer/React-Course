08d31c38e8cc105457ed9337c2d66a1130e0ce09
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-23 11:37:25 +0500

    refactor: update UseRefComponent for improved placeholder and button text

--- src/components/UseRefComponent.jsx
+++ src/components/UseRefComponent.jsx
@@ -2,17 +2,18 @@ import { useRef } from "react";
 
 function UseRefComponent() {
   const inputRef = useRef(null);
+
   const inputHandler = () => {
     inputRef.current.focus();
-    inputRef.current.placeholder = "This is Ref";
+    inputRef.current.placeholder = "Ref is active";
     inputRef.current.style.color = "red";
   };
 
   return (
     <div>
-      <h1>UseRef Hook in React</h1>
-      <input ref={inputRef} type="text" placeholder="Enter your name" />
-      <button onClick={inputHandler}>Focus</button>
+      <h1>React useRef Hook</h1>
+      <input ref={inputRef} type="text" placeholder="Enter your username" />
+      <button onClick={inputHandler}>Activate Ref</button>
     </div>
   );
 }
