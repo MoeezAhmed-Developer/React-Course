ab673439dfd389905327a5469ebbf1308d82420b
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-23 15:34:53 +0500

    fix: correct input placeholders in CustomerForm and remove commented-out validation code in UseFormHooks

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -2,12 +2,11 @@ import { useFormStatus } from "react-dom";
 
 function CustomerForm() {
   const { pending } = useFormStatus();
-  // console.log(pending);
 
   return (
     <div>
-      <input type="text" placeholder="neter usernme" id="username" />
-      <input type="password" placeholder="Enetr password" id="password" />
+      <input type="text" placeholder="Enter username" />
+      <input type="password" placeholder="Enter password" />
       <button disabled={pending}>
         {pending ? "Submitting...." : "Submit"}
       </button>
--- src/components/UseFormHooks.jsx
+++ src/components/UseFormHooks.jsx
@@ -2,17 +2,6 @@ import CustomerForm from "./CustomerForm";
 
 function UseFormHooks() {
   async function handleSubmit() {
-    // const username = document.querySelector("#username");
-    // const password = document.querySelector("#password");
-
-    // if (username.value == "") {
-    //   return alert("Please fill your username.");
-    // }
-
-    // if (password.value == "") {
-    //   return alert("Please fill your password.");
-    // }
-
     await new Promise((res) => setTimeout(res, 2000));
   }
 
