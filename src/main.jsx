bab96c9a8861aca4aa096933104564e10daf491b
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-25 23:49:14 +0500

    fix: uncomment title in index.html and remove StrictMode wrapper from main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,8 +7,6 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </BrowserRouter>,
 );
